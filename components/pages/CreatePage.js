750780f2f719e96128efa8b2385f69e482119076
amc <amcmax212@gmail.com>
2021-08-10 23:01:39 +0300

    CreatePage form styled with TailwindCSS

--- components/pages/CreatePage.js
+++ components/pages/CreatePage.js
@@ -31,48 +31,87 @@ export default function CreatePage() {
   );
 
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        console.log(inputs);
-        const res = await createPage(data);
-        console.log(res);
-      }}
-    >
-      <label htmlFor="name">
-        Name
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="description">
-        Description
-        <textarea
-          type="textarea"
-          id="description"
-          name="description"
-          placeholder="description"
-          value={inputs.description}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="url">
-        URL
-        <input
-          type="text"
-          id="url"
-          name="url"
-          placeholder="url"
-          value={inputs.url}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Add Page</button>
-    </form>
+    <div class="object-center w-full max-w-xl">
+      <form
+        class="bg-white rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(inputs);
+          const res = await createPage(data);
+          console.log(res);
+        }}
+      >
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="name"
+            >
+              Page Title
+            </label>
+          </div>
+
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="description"
+            >
+              Description
+            </label>
+          </div>
+
+          <textarea
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="textarea"
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="url"
+            >
+              URL
+            </label>
+          </div>
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            id="url"
+            name="url"
+            placeholder="url"
+            value={inputs.url}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Add Page
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
