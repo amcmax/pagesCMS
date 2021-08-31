5628e557c15a9a2d85504655849ebffcdf3107ee
amc <amcmax212@gmail.com>
2021-09-01 01:12:07 +0300

    background color fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
-import '../styles.css';
+import "../styles.css";
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <div class="h-screen bg-pink-50">
+      <div class="bg-pink-50">
         <Component {...pageProps} />
       </div>
     </ApolloProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
